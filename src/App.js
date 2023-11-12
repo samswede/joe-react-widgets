73a1bad8217ed1c8a30f8ab6d1bef4b19c74e8ac
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-11-12 18:56:43 +0100

    see all widgets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { Widget } from "./widgets/pagination/Pagination";
+import { Widget as Widget1} from "./widgets/widget-1/Widget";
+import { Widget as Widget2} from "./widgets/widget-2/Widget";
 
 function App() {
-  return <Widget />;
+  return <>
+  <Widget />
+  <Widget1 />
+  <Widget2 />
+  </>;
 }
 
 export default App;
